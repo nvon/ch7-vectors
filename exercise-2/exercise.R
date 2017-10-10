e8e2337da93818575d089e38b30b805778b9ba49
nvon <32344564+nvon@users.noreply.github.com>
2017-10-10 14:59:30 -0700

    Update exercise.R

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,14 +1,14 @@
 # Exercise 2: Subsetting and Manipulating Vectors
 
 # Create a vector `x` that contains the numbers 5,2,6,2,1,7
-
+x <- c(5,2,6,2,1,7)
 
 # Create a vector `y` that has the numbers 2,3
-
+y <- c(2,3)
 
 # Create a vector `z` by adding (not combining but arithmetically adding)
 # `x` and `y` (note the recycling!)
-
+z <- x+y
 
 ## Create a vector `first.three` that has the first three elements of `z` in it.
 ## Use (positional) indexing!
