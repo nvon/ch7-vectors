5d3ede5dc230714596eacd6570537ede4426f1e4
nvon <32344564+nvon@users.noreply.github.com>
2017-10-11 11:23:57 -0700

    Update exercise.R

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -7,20 +7,21 @@ x <- c(5,2,6,2,1,7)
 y <- c(2,3)
 
 # Create a vector `z` by adding (not combining but arithmetically adding)
-# `x` and `y` (note the recycling!)
+# `x` and `y` (note the recycling!) ( z = 7 5 8 5 3 10 )
 z <- x+y
+
 
 ## Create a vector `first.three` that has the first three elements of `z` in it.
 ## Use (positional) indexing!
-
+first.three <- 
 
 ## Create a vector `small` that has the values of `z` that are less than 5
-## Use logical indexing!
-
+## Use logical indexing! http://www.r-tutor.com/r-introduction/vector/logical-index-vector
+small <- z[c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE)]
 
 ## Create a vector `large` that has the values of `z` that are greater than or equal to 5
-## Use logical indexing!
-
+## Use logical indexing! http://www.r-tutor.com/r-introduction/vector/logical-index-vector
+large <- z[c(TRUE, TRUE, TRUE, TRUE, FALSE, TRUE)]
 
 ### Bonus ###
 
